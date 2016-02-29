32461b03613c12d486da399453c8250f15c22bab
happyyi-py <gfcocos@icloud.com>
2016-02-29 07:44:26 +0000

    fix datetime

--- log/admin.py
+++ log/admin.py
@@ -4,15 +4,11 @@ from .models import Log
 # Register your models here.
 class LogAdmin(admin.ModelAdmin):
     """docstring for LogAdmin"""
-    list_display = ('time_seconds', 'clinetip', 'retserver')
-
-    def time_seconds(self, obj):
-        return obj.time.strftime('%y-%m-%d %H:%M:%S')
+    list_display = ('time', 'clinetip', 'retserver')
 
     def __str__(self):              # __unicode__ on Python 2
         if self.time:
             t = self.time.strftime('%m-%d-%y %H:%M:%S')
         else:
             t = 'None'
-
 admin.site.register(Log, LogAdmin)
