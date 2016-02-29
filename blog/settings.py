fdc174191c2d01b0a2254f69e387d75fde848676
gfcocos <gfcocos@gmail.com>
2016-02-29 15:26:28 +0800

    change datetime format

--- blog/settings.py
+++ blog/settings.py
@@ -116,6 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+from django.conf.locale.en import formats as en_formats
+en_formats.DATETIME_FORMAT = "Y m d H:i:s"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
